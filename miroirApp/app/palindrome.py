b59ab7c721dfd040ba3fcc95cc19b09ff9702cf5
Julie Montoux <92477225+JulieMontoux@users.noreply.github.com>
2025-06-11 09:06:21 +0200

    [RED] inversement mot

--- miroirApp/app/palindrome.py
+++ miroirApp/app/palindrome.py
@@ -1,3 +1,3 @@
 class OHCE :
     def Palindrome(self, input: str):
-        return "Bonjour" +input[::1]+"Au revoir"
+        return "Bonjour" + input[::1] + "Au revoir"
--- miroirApp/tests/palindrome.py
+++ miroirApp/tests/palindrome.py
@@ -3,10 +3,20 @@ from app.palindrome import OHCE
 
 class TestOHCE(unittest.TestCase):
     def test_dit_bonjour_en_premier(self):
+        # Étant donné une instance de OHCE
         ohce = OHCE()
+        # Quand on saisit une chaîne quelconque
         result = ohce.Palindrome("chat")
-        self.assertTrue(result.startswith("Bonjour"))  # add assertion here
+        # Alors la réponse commence par "Bonjour"
+        self.assertTrue(result.startswith("Bonjour"))
 
+    def test_returns_input_reversed(self):
+        # Étant donné une instance de OHCE
+        ohce = OHCE()
+        # Quand on saisit "chat"
+        result = ohce.palindrome("chat")
+        # Alors la réponse contient "tahc"
+        self.assertIn("tahc", result)
 
 if __name__ == '__main__':
     unittest.main()
